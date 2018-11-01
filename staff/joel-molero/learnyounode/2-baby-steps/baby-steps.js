e9536a86fa1aad9e78277157dafbff3b7647f353
Joel Molero <joel.molero@MacBook-Pro-de-Adriana.local>
2018-11-01 13:27:40 +0100

    added files

---
+++ staff/joel-molero/learnyounode/2-baby-steps/baby-steps.js
@@ -0,0 +1,8 @@
+const argv = process.argv.slice(2)
+
+let sum = 0;
+
+//argv.forEach(num => sum += parseFloat(num))
+argv.forEach(num => sum += Number(num))
+
+console.log(sum)
---
+++ staff/joel-molero/learnyounode/5-filtered-ls/filtered.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const path = require('path')
+
+const [,,dir,ext] = process.argv
+
+fs.readdir(dir, (err, files) => {
+    if (err) throw err
+
+    files.forEach(file => {
+        if (path.extname(file) === `.${ext}`) console.log(file)
+    })
+})
